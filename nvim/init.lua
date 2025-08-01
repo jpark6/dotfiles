650f59218e6be656abde206703ad35c0374328b1
jpark6 <qkstjr09@gmail.com>
2025-08-01 11:58:19 +0900

    Feat: Update 20250801

--- nvim/init.lua
+++ nvim/init.lua
@@ -812,6 +812,19 @@ require("lazy").setup({
     config = function()
       require("nvim-surround").setup({})
     end
+  },
+  {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+      cursor_color = "#ff8800",
+      stiffness = 0.3,
+      trailing_stiffness = 0.1,
+      damping = 0.5,
+      trailing_exponent = 5,
+      never_draw_over_target = true,
+      hide_target_hack = true,
+      gamma = 1,
+    }
   }
 })
 -- autocmd
